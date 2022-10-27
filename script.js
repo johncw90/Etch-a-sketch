4de2a7680ad73facfcb14e4f9d268bb02fe33b16
John <johncw90@yahoo.com>
2022-10-27 14:30:58 -0500

    Implement resize function

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 let grid = document.querySelector(".grid");
+let resizeBtn = document.querySelector(".resize");
 
-for (let i = 0; i < 256; i++) {
-  let box = document.createElement("div");
-  box.classList.add("box");
-  grid.appendChild(box);
+// for (let i = 0; i < 256; i++) {
+//   box.addEventListener("mouseover", () => {
+//     box.style.backgroundColor = "red";
+//   });
+// }
 
-  box.addEventListener("mouseover", () => {
-    box.style.backgroundColor = "red";
-  });
+resizeBtn.addEventListener("click", () => {
+  let size = getSize();
+  createGrid(size);
+});
+
+function createGrid(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i = 0; i < numDivs; i++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    grid.appendChild(box);
+    box.addEventListener("mouseover", () => {
+      box.style.backgroundColor = "red";
+    });
+  }
 }
+
+function getSize() {
+  let input = prompt("1-100");
+}
+
+createGrid(96);
