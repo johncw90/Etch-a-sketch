178975a2c264dd328bf2357ab15aa7663c9e04d5
John <johncw90@yahoo.com>
2022-10-28 00:03:25 -0500

    Fix resize function, update UI

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 let grid = document.querySelector(".grid");
 let resizeBtn = document.querySelector(".resize");
 
-// for (let i = 0; i < 256; i++) {
-//   box.addEventListener("mouseover", () => {
-//     box.style.backgroundColor = "red";
-//   });
-// }
-
 resizeBtn.addEventListener("click", () => {
   let size = getSize();
   createGrid(size);
 });
 
 function createGrid(size) {
+  // Clears current grid
+  if (grid.innerHTML !== "") {
+    grid.innerHTML = "";
+  }
+
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  let numDivs = size * size;
+  let boxes = size * size;
 
-  for (let i = 0; i < numDivs; i++) {
+  for (let i = 0; i < boxes; i++) {
     let box = document.createElement("div");
     box.classList.add("box");
     grid.appendChild(box);
     box.addEventListener("mouseover", () => {
-      box.style.backgroundColor = "red";
+      box.style.backgroundColor = "#e17055";
     });
   }
 }
 
 function getSize() {
   let input = prompt("1-100");
+  if (input > 0 && input < 100) {
+    return input;
+  } else {
+    alert("Pick a number between 1-100");
+  }
 }
 
-createGrid(96);
+createGrid(16);
